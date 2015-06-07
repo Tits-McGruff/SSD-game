20e7ad7809664cca75abe9f37cf00f93c301ff05
Tits-McGruff <jlow8697@gmail.com>
2015-06-07 11:23:40 +1000

    Update SP_menu.py

--- SP_menu.py
+++ SP_menu.py
@@ -9,28 +9,29 @@ class sp_menu(object):
 
     def __init__(self, screen): #surface, color, x, y, length, height, width, text, text_color, font size
         self.screen = screen
-        self.human_button = Menu_Object.Button(screen, (135, 12, 12), 20, 120, 550, 429, 0, "", (0, 0, 0), 35) #creates the buttons
-
-        self.alien_button = Menu_Object.Button(screen, (135, 12, 12), 831, 120, 550, 429, 0, "", (0, 0, 0), 35)
-
-        self.back_button = Menu_Object.Button(screen, (135, 12, 12), 1000, 600, 100, 50, 0, "Back", (0, 0, 0), 20)
         
-        self.screen.blit(get_image('Faction.png').convert(), (0, 0, 1280, 720))
+        self.human_button = Menu_Object.Button(screen, (120, 120, 120), 20, 140, 429, 550, 0, "", (0, 0, 0), 35) #creates the buttons
+
+        self.alien_button = Menu_Object.Button(screen, (120, 120, 120), 831, 140, 429, 550, 0, "", (0, 0, 0), 35)
+
+        self.back_button = Menu_Object.Button(screen, (135, 12, 12), 565, 570, 150, 100, 0, "Back", (0, 0, 0), 35)
+        
     #draws them
     
     def draw(self):
+        self.screen.blit(get_image('assets\images\\backgrounds\Faction.png').convert(), (0, 0, 1280, 720))
         self.human_button.draw()
         self.alien_button.draw()
+        self.screen.blit(get_image('assets\images\\buttons\\human_button.png').convert(), (20, 140, 550, 429))
+        self.screen.blit(get_image('assets\images\\buttons\\alien_button.png').convert(), (831, 140, 550, 429))
         self.back_button.draw()
-        self.screen.blit(get_image('human button.png').convert(), (20, 120, 550, 429))
-        self.screen.blit(get_image('alien button.png').convert(), (831, 120, 550, 429))
         
     def handle_event(self, event):    
         if event.type == MOUSEBUTTONDOWN and self.human_button.pressed() == True:
-            print ("test")
+            print ("human")
     
         if event.type == MOUSEBUTTONDOWN and self.alien_button.pressed() == True:
-            print ("test")      
+            print ("alien")      
             
         if event.type == MOUSEBUTTONDOWN and self.back_button.pressed() == True:
-            return self.main_menu.menu(screen)
+            return "main_menu"
