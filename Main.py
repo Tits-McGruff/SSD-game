b38c39e3f75740d9ee797efe23faaf955e6729ba
Tits-McGruff <jlow8697@gmail.com>
2015-06-07 11:22:53 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,24 +1,38 @@
 import pygame
 from pygame.locals import *
-import Menu
 import Menu_Object
 import main_menu
+import SP_menu
+import TP_menu
+import settings_menu
+import highscores_menu
 from image_loader import get_image
+import os
 
 clock = pygame.time.Clock()
 done = False 
 pygame.init()
 screen = pygame.display.set_mode((1280,720),pygame.FULLSCREEN)
 current_menu = main_menu.menu(screen)
-        
-screen.blit(get_image('Main_Menu.png').convert(), (0, 0, 1280, 720))
     
 while not done:
     
     for event in pygame.event.get():
-        temp = current_menu.handle_event(event)
-        if temp is not None:
-            current_menu = temp
+        if current_menu.handle_event(event) == "main_menu":
+            current_menu = main_menu.menu(screen)
+        
+        elif current_menu.handle_event(event) == "SP_menu":
+            current_menu = SP_menu.sp_menu(screen)
+            
+        elif current_menu.handle_event(event) == "TP_menu":
+            current_menu = TP_menu.tp_menu(screen)
+            
+        elif current_menu.handle_event(event) == "settings":
+            current_menu = settings_menu.settings_menu(screen)
+            
+        elif current_menu.handle_event(event) == "highscores":
+            current_menu = highscores_menu.highscores_menu(screen)
+            
         if event.type == pygame.QUIT:
             done = True            
             
